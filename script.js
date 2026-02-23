88cce5f2fa0afce2162e31ddb1f6177781b5f65a
Marko <202842510+Dema2101@users.noreply.github.com>
2026-02-23 11:48:28 +0100

    inicijalizacija

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log("Hello world");
+
+const myLibrary = [];
+
+function Book(title,author,pages,read){
+this.title=title;
+this.author=author;
+this.pages=pages;
+this.read=read;
+this.id=crypto.randomUUID();
+}
+
+function addBookToLibrary(title,author,pages,read){
+const book = new Book(title,author,pages,read);
+myLibrary.push(book);
+}
+
+addBookToLibrary("1984", "George Orwell", 328, true);
+addBookToLibrary("Dune", "Frank Herbert", 604, false);
+console.log(myLibrary);
